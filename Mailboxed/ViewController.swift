c2819f2319452f2e4ff70091a40b97d0f29eebc3
Loredana Crisan <loredanasf@gmail.com>
2014-09-27 22:51:45 -0700

    Scroll view works

--- Mailboxed/ViewController.swift
+++ Mailboxed/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    @IBOutlet weak var messagesImage: UIImageView!
+    @IBOutlet weak var helpImage: UIImageView!
+    @IBOutlet weak var searchImage: UIImageView!
+    @IBOutlet weak var messageImage: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        // set size of scroll view
+        
+        var scrollHeight = CGFloat ()
+        scrollHeight = messagesImage.frame.size.height + helpImage.frame.size.height + searchImage.frame.size.height + messageImage.frame.size.height
+
+        feedScrollView.contentSize = CGSize (width: 320, height: scrollHeight)
+        
+        print(scrollHeight)
+        
+        print(feedScrollView.contentSize)
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
